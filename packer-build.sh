4daa664e9ee8d443fddd7e24ec41bd8ccb8489ef
Blaz Pirc <bpirc@ssrd.io>
2026-09-14 10:08:10 +0200

    fixed packer compilation disk position

--- packer-build.sh
+++ packer-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-packer build -only=proxmox-iso -var-file=./packer/templates/variables.json ./packer/templates/windows_2008_r2.json
+packer build -on-error=ask -only=proxmox-iso -var-file=./packer/templates/variables.json ./packer/templates/windows_2008_r2.json
